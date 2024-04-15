30111e59407ef1276922a7c2b0bcd6db8b06d863
yassine essadi <yasine.esadi@gmail.com>
2024-04-15 18:51:35 +0100

    handle the status code and check if the code is 200

--- include/web_parser.py
+++ include/web_parser.py
@@ -3,5 +3,6 @@ from bs4 import BeautifulSoup
 
 def parse_website(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    return soup
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup
