88f83e29e34960abbef94887ae63349a40521c8e
yassine essadi <yasine.esadi@gmail.com>
2024-04-16 18:40:56 +0100

    get the url and the header from google results

--- include/get_elements.py
+++ include/get_elements.py
@@ -1,42 +1,47 @@
 import time
-from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 class GetElements:
     @staticmethod
-    def get_search_results(driver, search_engine):
-        search_results_headers = []
-        scrolls = 0
-        max_scrolls = 10
-        last_scroll_height = driver.execute_script("return document.body.scrollHeight")
+    def get_page_source(driver):
+        return driver.page_source
 
+    @staticmethod
+    def scroll_page(driver, max_scrolls=10):
+        last_scroll_height = driver.execute_script("return document.body.scrollHeight")
+        scrolls = 0
         while scrolls < max_scrolls:
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(3)
-
-            if search_engine == 'google':
-                try:
-                    h3_elems = driver.find_elements(By.XPATH, "//h3")
-                    for h3_elem in h3_elems:
-                        title = h3_elem.text
-                        if title:
-                            search_results_headers.append(title)
-                except Exception as e:
-                    print(f"Error extracting data from Google search result: {e}")
-            elif search_engine == 'yandex':
-                try:
-                    h2_elems = driver.find_elements(By.XPATH, '//h2')
-                    for h2_elem in h2_elems:
-                        result_text = h2_elem.text
-                        if result_text:
-                            search_results_headers.append(result_text)
-                except Exception as e:
-                    print(f"Error extracting text from Yandex search result: {e}")
-
             new_scroll_height = driver.execute_script("return document.body.scrollHeight")
             if new_scroll_height == last_scroll_height:
                 break
-
             last_scroll_height = new_scroll_height
             scrolls += 1
 
-        return search_results_headers
+    @staticmethod
+    def extract_elements(source_page, search_engine):
+        search_results = []
+        
+        soup = BeautifulSoup(source_page, 'html.parser')
+
+        if search_engine == 'google':
+            for container in soup.find_all(class_='yuRUbf'):
+                title = container.find('h3').text.strip()
+                link = container.find('a')['href']
+                if title and link:
+                    search_results.append({
+                        'Title': title,
+                        'Link': link
+                    })
+        elif search_engine == 'yandex':
+            try:
+                h2_elems = soup.find_all('h2')
+                for h2_elem in h2_elems:
+                    result_text = h2_elem.text
+                    if result_text:
+                        search_results.append(result_text)
+            except Exception as e:
+                print(f"Error extracting text from Yandex search result: {e}")
+        
+        return search_results
