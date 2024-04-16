d5fd44fe9cbf833b190a10b5cf62946b36dd36ea
yassine essadi <yasine.esadi@gmail.com>
2024-04-16 18:42:49 +0100

    i don't remember anay change here (-.-)

--- include/currency_exchange_rate.py
+++ include/currency_exchange_rate.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 from bs4 import BeautifulSoup
 
 class CurrencyExchangeRate:
