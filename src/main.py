4c974c79d4270ffb2a3c264b5c7a19f833c00510
yassine essadi <yasine.esadi@gmail.com>
2024-04-16 18:40:25 +0100

    call the functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
-sys.path.append('../')  
+sys.path.append('../')
 
-from include import web_driver, data_handler, get_elements, search_engine, helper, currency_exchange_rate,captcha_handler,user_input
+from include import web_driver, data_handler, get_elements, search_engine, helper, currency_exchange_rate, captcha_handler, user_input
 
 class MainApp:
     def run(self):
@@ -14,26 +14,29 @@ class MainApp:
 
         queries_list = helper.Helper.get_query_list(queries)
         all_search_results = []
-        
+
         for query in queries_list:
             if choice == '1':
                 search_engine.SearchEngine(driver).search_google(query)
                 # Check for Google CAPTCHA and wait for manual solving
-                captcha_handler.CaptchaHandler.handle_captcha(driver)
-                search_results_headers = get_elements.GetElements.get_search_results(driver, 'google')
+                source_page = get_elements.GetElements.get_page_source(driver)
+                captcha_handler.CaptchaHandler.handle_captcha(source_page, 'google')
+                get_elements.GetElements.scroll_page(driver)
+                source_page = get_elements.GetElements.get_page_source(driver)
+                search_results = get_elements.GetElements.extract_elements(source_page, 'google')
             elif choice == '2':
                 search_engine.SearchEngine(driver).search_yandex(query)
-                search_results_headers = get_elements.GetElements.get_search_results(driver, 'yandex')
+                # Check for Yandex CAPTCHA and wait for manual solving
+                source_page = get_elements.GetElements.get_page_source(driver)
+                captcha_handler.CaptchaHandler.handle_captcha(source_page, 'yandex')
+                get_elements.GetElements.scroll_page(driver)
+                source_page = get_elements.GetElements.get_page_source(driver)
+                search_results = get_elements.GetElements.extract_elements(source_page, 'yandex')
             else:
                 print("Invalid choice. Please try again.")
                 sys.exit(1)
-            all_search_results.extend(search_results_headers)
-        
-        # Print all collected search result headers
-        print("\nCollected Search Result Headers:")
-        for idx, heading in enumerate(all_search_results, start=1):
-            print(f"{idx}. {heading}")
-        
+            all_search_results.extend(search_results)
+
         # Save the collected data to a CSV file
         data_handler.DataHandler.save_to_csv(all_search_results)
 
