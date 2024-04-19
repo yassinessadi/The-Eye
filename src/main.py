76264ab61ed9d0fdf927450d747949b4ad270508
= <yasine.esadi@gmail.com>
2024-04-20 00:35:02 +0100

    save json file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 sys.path.append('../')
 
 from include import web_driver, data_handler, get_elements, search_engine, helper, currency_exchange_rate, captcha_handler, user_input
@@ -23,7 +24,7 @@ class MainApp:
                 captcha_handler.CaptchaHandler.handle_captcha(source_page, 'google')
                 get_elements.GetElements.scroll_page(driver)
                 source_page = get_elements.GetElements.get_page_source(driver)
-                search_results = get_elements.GetElements.extract_elements(source_page, 'google')
+                search_results_json = get_elements.GetElements.extract_elements(source_page, 'google')
             elif choice == '2':
                 search_engine.SearchEngine(driver).search_yandex(query)
                 # Check for Yandex CAPTCHA and wait for manual solving
@@ -31,14 +32,17 @@ class MainApp:
                 captcha_handler.CaptchaHandler.handle_captcha(source_page, 'yandex')
                 get_elements.GetElements.scroll_page(driver)
                 source_page = get_elements.GetElements.get_page_source(driver)
-                search_results = get_elements.GetElements.extract_elements(source_page, 'yandex')
+                search_results_json = get_elements.GetElements.extract_elements(source_page, 'yandex')
             else:
                 print("Invalid choice. Please try again.")
                 sys.exit(1)
-            all_search_results.extend(search_results)
+            
+            search_results = json.loads(search_results_json)
+            search_results['SearchEngine'] = 'Google' if choice == '1' else 'Yandex'
+            all_search_results.append(search_results)
 
-        # Save the collected data to a CSV file
-        data_handler.DataHandler.save_to_csv(all_search_results)
+        # Save the collected data to a JSON file
+        data_handler.DataHandler.save_to_json(all_search_results)
 
 if __name__ == "__main__":
     app = MainApp()
