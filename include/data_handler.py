cee60a78b398fe64aea9392476b4f3f7ac6042b4
= <yasine.esadi@gmail.com>
2024-04-20 00:35:44 +0100

    save the file as json and using encoding to read arabic files

--- include/data_handler.py
+++ include/data_handler.py
@@ -2,6 +2,19 @@ import pandas as pd
 
 class DataHandler:
     @staticmethod
-    def save_to_csv(data, filename="../data/output.csv"):
-        df = pd.DataFrame(data)
-        df.to_csv(path_or_buf=filename)
+    def save_to_json(all_search_results, filename="../data/output.json"):
+        df_list = []
+        
+        for result in all_search_results:
+            for info in result['info']:
+                df_info = pd.DataFrame([info])  # Removed index=[0]
+                df_info['SearchEngine'] = result['SearchEngine']
+                df_list.append(df_info.to_dict(orient='records'))
+                
+            for question in result['questions']:
+                df_questions = pd.DataFrame({'Question': [question]})
+                df_questions['SearchEngine'] = result['SearchEngine']
+                df_list.append(df_questions.to_dict(orient='records'))
+                
+        df_combined = pd.DataFrame(df_list)
+        df_combined.to_json(path_or_buf=filename, orient='records', lines=True, force_ascii=False)
