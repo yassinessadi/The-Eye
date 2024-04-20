5ca6c54505eee845535062320300c543a53137d6
= <yasine.esadi@gmail.com>
2024-04-20 11:54:26 +0100

    optimize the code (convert to functions)

--- include/user_input.py
+++ include/user_input.py
@@ -14,31 +14,38 @@ class UserInput:
         query = ""
 
         if choice == "1":
-            user_choice_currency = input("Do you want the current data exchange rate? (yes/no): ").lower()
-            if user_choice_currency == "yes":
-                Currency_Field_1 = input("Enter the Currency Amount Field 1: ")
-                Currency_Field_2 = input("Enter the Currency Amount Field 2: ")
-                query = Helper.build_currency_query(Currency_Field_1, Currency_Field_2)
-                active_converter = True
-            else:
-                user_choice_query = input("Do you want to enter a custom query? (yes/no): ").lower()
-                if user_choice_query == "yes":
-                    print("\nDo you want to use a custom query? ->Please make sure you are using ',' between the query to allow multi-search in Google result. example: intext:'orange' site:fruit.com ,inurl:'orange' site:exmple.com\n")
-                    query = Helper.custom_query()
-                else:
-                    domain = input("Enter the domain: ")
-                    keyword = input("Enter the keyword: ")
-                    query = Helper.build_dorks_query(domain, keyword)
+            query, active_converter = UserInput.google_input()
         elif choice == "2":
-            user_choice_query = input("Do you want to enter a custom query? (yes/no): ").lower()
-            if user_choice_query == "yes":
-                query = Helper.custom_query()
-            else:
-                domain = input("Enter the domain: ")
-                keyword = input("Enter the keyword: ")
-                query = Helper.build_dorks_query(domain, keyword)
-        else:
-            print("Invalid choice. Please try again.")
-            sys.exit(1)
-
+            query = UserInput.yandex_input()
+        
         return choice, query, active_converter
+
+    @staticmethod
+    def google_input():
+        user_choice_currency = input("Do you want the current data exchange rate? (yes/no): ").lower()
+        
+        if user_choice_currency == "yes":
+            Currency_Field_1 = input("Enter the Currency Amount Field 1: ")
+            Currency_Field_2 = input("Enter the Currency Amount Field 2: ")
+            return Helper.build_currency_query(Currency_Field_1, Currency_Field_2), True
+        
+        user_choice_query = input("Do you want to enter a custom query? (yes/no): ").lower()
+        
+        if user_choice_query == "yes":
+            print("\nDo you want to use a custom query? ->Please make sure you are using ',' between the query to allow multi-search in Google result. example: intext:'orange' site:fruit.com ,inurl:'orange' site:exmple.com\n")
+            return Helper.custom_query(), False
+        
+        domain = input("Enter the domain: ")
+        keyword = input("Enter the keyword: ")
+        return Helper.build_dorks_query(domain, keyword), False
+
+    @staticmethod
+    def yandex_input():
+        user_choice_query = input("Do you want to enter a custom query? (yes/no): ").lower()
+        
+        if user_choice_query == "yes":
+            return Helper.custom_query()
+        
+        domain = input("Enter the domain: ")
+        keyword = input("Enter the keyword: ")
+        return Helper.build_dorks_query(domain, keyword), False
