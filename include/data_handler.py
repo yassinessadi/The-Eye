2d9c1714415c7bf0489e41f6b3fdc51d5c55415d
= <yasine.esadi@gmail.com>
2024-05-30 16:46:15 +0100

    change the passed data

--- include/data_handler.py
+++ include/data_handler.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 class DataHandler:
     @staticmethod
-    def save_to_json(all_search_results, filename="../data/output.json"):
-        df_results = pd.DataFrame(all_search_results)
+    def save_to_json(data, filename="../data/output.json"):
+        df_results = pd.DataFrame(data)
         df_results.to_json(path_or_buf=filename, orient='records', force_ascii=False)
